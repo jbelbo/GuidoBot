c9ea4d525e44a46264e68db1e0a635746bcaabeb
Guido Genzone <ggenzone@protonmail.com>
2021-11-16 00:36:11 +0100

    Remove prints

--- commands/add.go
+++ commands/add.go
@@ -1,13 +1,11 @@
 package Commands
 
 import (
-    "fmt"
 	"jbelbo/guidoBot/telegram"
 )
 
 func Add(body *Telegram.WebhookReqBody, responseBody *Telegram.MessageResponse) error {
 
-    fmt.Println("Datos +v", body )
 	responseBody.Text = "/add: Not implemented."
 	return nil
 }

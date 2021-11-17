028a31af6ee6761342b8346104803e028ec86c2a
Guillermo Tomé <guillermo.tome@gmail.com>
2021-11-17 10:19:15 -0300

    mongo connection

--- commands/random.go
+++ commands/random.go
@@ -1,13 +1,38 @@
 package Commands
 
 import (
+	"context"
 	"database/sql"
-	"jbelbo/guidoBot/telegram"
+	Telegram "jbelbo/guidoBot/telegram"
 	"log"
 	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func RandomStuff(responseBody *Telegram.MessageResponse) error {
+
+	clientOptions := options.Client().
+		ApplyURI(os.Getenv("MONGO_URL"))
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection := client.Database("messages").Collection("originals")
+
+	res, err := collection.InsertOne(context.Background(), bson.M{"hello": "world"})
+	if err != nil {
+		return err
+	}
+	id := res.InsertedID.(primitive.ObjectID).String()
+
 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatalf("Error opening database: %q", err)
@@ -23,6 +48,7 @@ func RandomStuff(responseBody *Telegram.MessageResponse) error {
 
 	for results.Next() {
 		var err = results.Scan(&responseBody.Text)
+		responseBody.Text = responseBody.Text + " " + id
 		if err != nil {
 			log.Fatal("Error while reading from row")
 		}

cfc562b7e28222ae82b00425e578d5a7f0942cf7
Guido Genzone <ggenzone@protonmail.com>
2021-11-17 17:12:42 +0100

    Add logic to persist in /add command

--- commands/add.go
+++ commands/add.go
@@ -1,11 +1,39 @@
 package Commands
 
 import (
+	"context"
 	"jbelbo/guidoBot/telegram"
+	"log"
+	"os"
+	"time"
+	"strings"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func Add(body *Telegram.WebhookReqBody, responseBody *Telegram.MessageResponse) error {
 
-	responseBody.Text = "/add: Not implemented."
+	text := strings.TrimPrefix(body.Message.Text, "/add ")
+
+	clientOptions := options.Client().ApplyURI(os.Getenv("MONGO_URL"))
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection := client.Database("messages").Collection("custom")
+
+	res, err := collection.InsertOne(context.Background(), bson.M{"txt": text})
+	if err != nil {
+		return err
+	}
+	id := res.InsertedID.(primitive.ObjectID).String()
+
+
+	responseBody.Text = "/add: " + id
 	return nil
 }

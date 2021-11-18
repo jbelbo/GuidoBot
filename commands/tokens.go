d9b5475b4746b5e66cd88e9fa5bf039fe97e7233
Santiago Gonzalez <nojero@gmail.com>
2021-11-18 17:51:25 -0300

    Adding option for symbols to be passed to tokens command (#7)
    feat: add option for symbols to be passed to tokens command

--- commands/tokens.go
+++ commands/tokens.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"jbelbo/guidoBot/telegram"
 	"net/http"
+	"strings"
 )
 
 type Token struct {
@@ -14,14 +15,57 @@ type Token struct {
 	ID     string
 	Symbol string
 	Name   string
+	Ath    float32
+	Atl    float32
+	Vol    int64 `json:"total_volume"`
+}
+
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+
+	return false
+}
+
+func formatAllTokens(responseBody *Telegram.MessageResponse, tokens []Token) {
+	responseBody.Text = "= Lista: \n"
+	for _, value := range tokens {
+		responseBody.Text = responseBody.Text + value.Name + "(" + value.Symbol + ") = " + fmt.Sprintf("%f", value.Price) + "\n"
+	}
+}
+
+func formatListedTokens(responseBody *Telegram.MessageResponse, tokens []Token, params string) {
+	coins := strings.Split(strings.ToUpper(params), " ")
+	var requestedTokens []Token
+
+	for _, value := range tokens {
+		if contains(coins, strings.ToUpper(value.Symbol)) {
+			requestedTokens = append(requestedTokens, value)
+		}
+	}
+	if len(requestedTokens) == 0 {
+		return
+	}
+
+	responseBody.Text = "= Detalles: \n"
+	for _, token := range requestedTokens {
+		responseBody.Text = responseBody.Text + token.Name + "(" + token.Symbol + ") :\n"
+		responseBody.Text = responseBody.Text + "    - Price: " + fmt.Sprintf("%f", token.Price) + "\n"
+		responseBody.Text = responseBody.Text + "    - ATL: " + fmt.Sprintf("%f", token.Atl) + " - ATH: " + fmt.Sprintf("%f", token.Ath) + "\n"
+		responseBody.Text = responseBody.Text + "    - VOL: " + fmt.Sprintf("%d", token.Vol) + "\n"
+	}
 }
 
 // List Token from API
 //
-func ListTokens(responseBody *Telegram.MessageResponse) error {
+func ListTokens(reqBody *Telegram.WebhookReqBody, responseBody *Telegram.MessageResponse) error {
 
 	//https://www.sohamkamani.com/golang/json/
 	//https://dev.to/billylkc/parse-json-api-response-in-go-10ng
+	params := strings.TrimSpace(strings.TrimPrefix(reqBody.Message.Text, "/tokens"))
 
 	resp, err := http.Get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd")
 	if err != nil {
@@ -34,11 +78,12 @@ func ListTokens(responseBody *Telegram.MessageResponse) error {
 	if err == nil {
 		var tokens []Token
 		json.Unmarshal([]byte(body), &tokens)
-		fmt.Printf("Tokens : %+v", tokens)
-		responseBody.Text = "= Lista: \n"
+		//fmt.Printf("Tokens : %+v", tokens)
 
-		for _, value := range tokens {
-			responseBody.Text = responseBody.Text + value.Name + "(" + value.Symbol + ") = " + fmt.Sprintf("%f", value.Price) + "\n"
+		if len(params) == 0 {
+			formatAllTokens(responseBody, tokens)
+		} else {
+			formatListedTokens(responseBody, tokens, params)
 		}
 	}
 

c93bcf8603c60ab856816a1ea02966b0e380ad82
E. Rivas <224161+erivas@users.noreply.github.com>
2022-06-15 19:57:41 -0300

    feat: add flags to /matches

--- commands/worldcup.go
+++ commands/worldcup.go
@@ -27,6 +27,32 @@ type Match struct {
 	Group       string             `bson:"Group,omitempty"`
 }
 
+type Flag struct {
+	Name  string `bson:"name,omitempty"`
+	Emoji string `bson:"emoji,omitempty"`
+}
+
+func LookupFlag(country string) string {
+	clientOptions := options.Client().ApplyURI(os.Getenv("MONGO_URL"))
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection := client.Database("worldcup").Collection("flags")
+	fmt.Print(country)
+	filter := bson.D{{"name", bson.D{{"$regex", regexp.QuoteMeta(country)}, {"$options", "i"}}}}
+
+	var result Flag
+	if err = collection.FindOne(ctx, filter).Decode(&result); err != nil {
+		return ""
+	}
+
+	return result.Emoji
+}
+
 func MatchesForTeam(reqBody *Telegram.WebhookReqBody, responseBody *Telegram.MessageResponse) error {
 	team := strings.TrimSpace(strings.TrimPrefix(reqBody.Message.Text, "/matches"))
 
@@ -59,7 +85,7 @@ func MatchesForTeam(reqBody *Telegram.WebhookReqBody, responseBody *Telegram.Mes
 
 	var out strings.Builder
 	for _, result := range results {
-		out.WriteString(fmt.Sprintf("On %s: %s vs. %s in %s\n", result.DateUtc, result.HomeTeam, result.AwayTeam, result.Location))
+		out.WriteString(fmt.Sprintf("On %s: %s(%s) vs. %s(%s) in %s\n", result.DateUtc, result.HomeTeam, LookupFlag(result.HomeTeam), result.AwayTeam, LookupFlag(result.AwayTeam), result.Location))
 	}
 	responseBody.Text = out.String()
 

8461e896588e020384c7157686653939b3d03a53
jbelbo <spinarolli.matias@gmail.com>
2022-06-18 17:00:27 -0300

    fix: look for commands before keywords to avoid incorrect responses

--- handler/run.go
+++ handler/run.go
@@ -11,39 +11,72 @@ import (
 	"regexp"
 )
 
-// Decode and Parse
 func Run(res http.ResponseWriter, req *http.Request) {
-	// First, decode the JSON response body
 	body := &Telegram.WebhookReqBody{}
 
 	if err := json.NewDecoder(req.Body).Decode(body); err != nil {
-		fmt.Println("could not decode request body", err)
+		fmt.Println("Could not decode request body", err)
 		return
 	}
 
 	if err := parseRequest(body); err != nil {
-		fmt.Println("error in sending reply:", err)
+		fmt.Println("Error while sending reply: ", err)
 		return
 	}
 
-	fmt.Println("reply sent")
+	fmt.Println("Reply sent")
 }
 
 // Available commands:: /help /add /random /tokens /send /status @user_mention
 func parseRequest(body *Telegram.WebhookReqBody) error {
-
 	responseBody := Telegram.MessageResponse{
 		ChatID: body.Message.Chat.ID,
 		Text:   "",
 	}
 
-	if botReconizeKeyword(body) {
+	regex := regexp.MustCompile("^\\/[a-zA-Z]*")
+	command := regex.FindString(body.Message.Text)
+
+	if command != "" {
+		var err error
+
+		switch command {
+		case "/random":
+			err = Commands.RandomStuff(&responseBody)
+		case "/tokens":
+			err = Commands.ListTokens(body, &responseBody)
+		case "/add":
+			err = Commands.Add(body, &responseBody)
+		case "/status":
+			err = Commands.Status(body, &responseBody)
+		case "/send":
+			err = Commands.Send(body, &responseBody)
+		case "/help":
+			err = Commands.Help(&responseBody)
+		case "/weather":
+			err = Commands.GetWeather(body, &responseBody)
+		case "/joke":
+			err = Commands.GetJoke(body, &responseBody)
+		case "/matches":
+			err = Commands.MatchesForTeam(body, &responseBody)
+		default:
+			err = nil
+		}
+
+		if err != nil {
+			log.Fatal("Error while processing command")
+		}
+
+		return Telegram.SendResponse(body.Message.Chat.ID, &responseBody)
+	}
+
+	if recognizeKeyword(body) {
 		var err = Commands.RandomStuffWithKeyword(body, &responseBody)
 		if err != nil {
 			log.Fatal("Error in random quote with keyword command")
-		} else {
-			return Telegram.SendResponse(body.Message.Chat.ID, &responseBody)
 		}
+
+		return Telegram.SendResponse(body.Message.Chat.ID, &responseBody)
 	}
 
 	if messageContainsMention(body.Message.Entities) {
@@ -51,37 +84,8 @@ func parseRequest(body *Telegram.WebhookReqBody) error {
 		if err != nil {
 			log.Fatal("Error in mention command")
 		}
-	}
 
-	regex := regexp.MustCompile("^\\/[a-zA-Z]*")
-	command := regex.FindString(body.Message.Text)
-	var err error
-
-	switch command {
-	case "/random":
-		err = Commands.RandomStuff(&responseBody)
-	case "/tokens":
-		err = Commands.ListTokens(body, &responseBody)
-	case "/add":
-		err = Commands.Add(body, &responseBody)
-	case "/status":
-		err = Commands.Status(body, &responseBody)
-	case "/send":
-		err = Commands.Send(body, &responseBody)
-	case "/help":
-		err = Commands.Help(&responseBody)
-	case "/weather":
-		err = Commands.GetWeather(body, &responseBody)
-	case "/joke":
-		err = Commands.GetJoke(body, &responseBody)
-	case "/matches":
-		err = Commands.MatchesForTeam(body, &responseBody)
-	default:
-		err = nil
-	}
-
-	if err != nil {
-		log.Fatal("Error in command")
+		return Telegram.SendResponse(body.Message.Chat.ID, &responseBody)
 	}
 
 	if responseBody.Text == "" {
@@ -101,9 +105,8 @@ func messageContainsMention(entities []Telegram.MessageEntity) bool {
 	return false
 }
 
-func botReconizeKeyword(body *Telegram.WebhookReqBody) bool {
-
-	matched, _ := regexp.MatchString(`crypto|ETH|BTC`, body.Message.Text)
+func recognizeKeyword(body *Telegram.WebhookReqBody) bool {
+	matched, _ := regexp.MatchString(`(?i)crypto|ETH|BTC`, body.Message.Text)
 
 	return matched
 }
